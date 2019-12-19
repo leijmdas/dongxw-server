b0b7d788d8a86d9973d1e938c4b3a30af87a08cf
leijmdas@163.com <leijmdas@163.com>
2019-12-19 17:45:32 +0800

    add

---
+++ dongxw-service/src/main/java/com/kunlong/service/dongxw/consts/ManagerConsts.java
@@ -0,0 +1,19 @@
+package com.kunlong.service.dongxw.consts;
+/**
+ * 
+ * @className: DongxwConsts
+ * @description: 应用基本配置常量 
+ * @author ljm
+ * @date 2018年4月18日  
+ *
+ */
+public class ManagerConsts {
+
+	
+	protected static String[] MYBATIS_MAPPER_LOCATIONS = new String[] {"classpath*:/dbconfig/dongxw/*.xml"};
+	
+	protected static String MYBATIS_BASE_PACKAGE = "com.kunlong.service.dongxw.dao";
+	
+	
+	
+}
